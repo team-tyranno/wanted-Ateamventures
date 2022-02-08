9633bb58af96e34132fda4e0ae2c1851b78d9213
Seoyoung Jeong <issyo1022@gmail.com>
2022-02-09 05:02:21 +0900

    design: tabIndex에 의한 outline 제거

--- src/components/atoms/SelectBox/style.ts
+++ src/components/atoms/SelectBox/style.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   position: relative;
+  outline: none;
 `;
 
 export const DropDown = styled.div<{ isSelected: boolean }>`
