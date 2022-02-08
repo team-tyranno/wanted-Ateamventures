442c55cad326cb60c63a79047e1cf6d0a454c7c7
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-02-08 16:31:44 +0900

    feat: CardSection 컴포넌트 분리 및 마크업

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,13 @@
+// Atoms
+export { Toggle } from 'components/atoms/Toggle';
 export { Divider } from 'components/atoms/Divider';
 export { MenuItem } from 'components/atoms/MenuItem';
 export { Logo } from 'components/atoms/Logo';
 
+// Organisms
 export { Gnb } from 'components/organisms/Gnb';
-
+export { ButtonSection } from 'components/organisms/ButtonSection';
+export { InfoSection } from 'components/organisms/InfoSection';
+export { CardSection } from 'components/organisms/CardSection';
+// Templates
 export { DashBoard } from 'components/templates/DashBoard';
---
+++ src/components/organisms/CardSection/style.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+  /* @media (min-width: 1440px) {
+    width: 1130.01px;
+  }
+
+  @media (max-width: 1440px) and (min-width: 903.01px) {
+    width: 748.01px;
+  }
+
+  @media (max-width: 903.01px) and (min-width: 521px) {
+    width: 366px;
+  } */
+
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  margin-top: 32px;
+
+  /* position: absolute;
+  left: 155px;
+  top: 262px; */
+
+  border: 1px solid black;
+`;
