9d53b0682d4d7fd1797f3acf362265028d91d118
jambottle <jaewon.walks@gmail.com>
2022-02-09 06:45:58 +0900

    feat: 토글/필터링 조건에 부합하는 경우가 없으면, 안내문 표시하는 기능 구현

--- src/components/organisms/CardSection/style.tsx
+++ src/components/organisms/CardSection/style.tsx
@@ -8,3 +8,21 @@ export const Container = styled.section`
   width: 100%;
   margin-top: 32px;
 `;
+
+export const NoResult = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  height: 100px;
+  border: 1px solid #c2c2c2;
+
+  span {
+    color: #939fa5;
+    font-family: 'Noto Sans KR Regular';
+    font-size: 14px;
+    line-height: 20px;
+  }
+`;
