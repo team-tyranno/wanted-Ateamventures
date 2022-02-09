d275c7593643875d455c8d549557bb47138d355c
jambottle <jaewon.walks@gmail.com>
2022-02-09 09:40:01 +0900

    style: CardItem 컴포넌트의 너비를 유지하면서 반응형 레이아웃 구현

--- src/components/organisms/CardSection/style.tsx
+++ src/components/organisms/CardSection/style.tsx
@@ -3,11 +3,30 @@ import styled from 'styled-components';
 export const Container = styled.section`
   display: grid;
   gap: 16px;
-  grid-template-columns: repeat(auto-fit, minmax(340px, 1fr));
 
   width: 100%;
   margin-top: 32px;
   margin-bottom: 100px;
+
+  @media (min-width: 1700px) {
+    min-width: 1512px;
+    grid-template-columns: repeat(auto-fit, 366px);
+  }
+
+  @media (min-width: 1270px) and (max-width: 1699px) {
+    width: 1130px;
+    grid-template-columns: repeat(3, 366px);
+  }
+
+  @media (min-width: 840px) and (max-width: 1269px) {
+    width: 748px;
+    grid-template-columns: repeat(2, 366px);
+  }
+
+  @media (max-width: 839px) {
+    width: 366px;
+    grid-template-columns: repeat(1, 366px);
+  }
 `;
 
 export const NoResult = styled.section`
@@ -18,6 +37,7 @@ export const NoResult = styled.section`
 
   width: 100%;
   height: 100px;
+  margin-top: 32px;
   border: 1px solid #c2c2c2;
 
   span {
